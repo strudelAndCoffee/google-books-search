67e7a046b023456ce8c40a34137a9228579a4d27
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-22 17:55:46 -0500

    testing server for graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         createUser(username: String!, email: String!, password: String!): Auth
-        saveBook(BookData): User
+        saveBook(input: BookData): User
         removeBook(bookId: String): User
     }
 `;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {authMiddleware} = require('./utils/auth');
 
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 // define apollor server
@@ -22,12 +22,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.use(routes);
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
+// app.use(routes);
 
 // create instance of apollo server
 const startApolloServer = async (typeDefs, resolvers) => {
