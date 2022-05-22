d8ef3fccdf8b7c151100503ea9bafab1ba23a37d
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-22 16:13:30 -0500

    added me query and create user mutation in resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,30 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('savedBooks')
+
+                return userData;
+            }
+
+            throw new AuthenticationError("You are not logged in!");
+        }
+    },
+
+    Mutation: {
+        createUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        }
+    }
+};
+
+module.exports = resolvers;
