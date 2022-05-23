7e628616b87df41a06bc7ebdc863711265680fe2
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-22 22:24:00 -0500

    added queries.js with me query to client

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// og = mongodb://localhost/googlebooks
+// different = mongodb://127.0.0.1:27017/googlebooks
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
-// import apollo server
 const {ApolloServer} = require('apollo-server-express');
-// import GraphQL schemas
 const { typeDefs, resolvers } = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 
@@ -10,7 +8,6 @@ const db = require('./config/connection');
 // const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
-// define apollor server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -22,13 +19,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // app.use(routes);
 
