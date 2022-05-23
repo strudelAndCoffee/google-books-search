c7728c28cd559d57ad78b75fb2d9dc1fb8d5ab5e
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-23 02:14:42 -0500

    fixed syntax for me query in server

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // og = mongodb://localhost/googlebooks
 // different = mongodb://127.0.0.1:27017/googlebooks
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,7 @@ const typeDefs = gql`
 
     type Mutation {
         login(email: String!, password: String!): Auth
-        createUser(username: String!, email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
         saveBook(bookId: String!, authors: [String], description: String, title: String, image: String, link: String): User
         removeBook(bookId: String!): User
     }
